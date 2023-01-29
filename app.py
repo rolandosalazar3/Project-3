2a5843556dac91e6b1b724b9b59e71d306afb58a
rolandosalazar3 <113559238+rolandosalazar3@users.noreply.github.com>
2023-01-29 16:51:10 -0600

    app.py updated

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
 from flask import Flask, render_template, jsonify
 import pandas
 import json
+import numpy as np
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    team_list = ["Jumpers", "Dunkers", "Dribblers", "Passers"]
-    return render_template("index.html", list=team_list)
+    return render_template("index.html")
+
+@app.route("/founded")
+def founded():
+    return render_template("founded.html")
+
+
+@app.route("/appNotes.json")
+def app_notes():
+    df = pandas.read_csv('university_data.csv')
+    
+    data = {
+        'features': [{cval : str(df.iloc[row, list(df.columns).index(cval)]) for cval in df.iloc[[row]]} for row in range(len(df))]
+    }
+    return jsonify(data)
 
 @app.route("/api/v1.0/data")
 def source_data():
-    df=pandas.read_csv ("university_data.csv")
+    df=pandas.read_csv("university_data.csv").dropna()
     data = {x: df[x].tolist() for x in df.columns}
     return jsonify(data)
 
@@ -27,7 +41,14 @@ def lat_long():
     return jsonify(data)
 
 
-    
+@app.route("/api/v1.0/Collage_Name")
+def Collage_Name():
+    df=pandas.read_csv ("university_data.csv")
+    data = {
+      'Collage_Name': df["Name"].tolist(),
+      'another_col': ['data_here']
+    }
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
